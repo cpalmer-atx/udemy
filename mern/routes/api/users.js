6cdae220c34ba56f4a1c2f6fb62120c05b9a4958
Chad Palmer <chad.n.palmer@gmail.com>
2021-09-13 17:22:54 -0500

    basic user route functionality added with input validation

--- mern/routes/api/users.js
+++ mern/routes/api/users.js
@@ -1,9 +1,31 @@
 const express = require('express');
+const { check, validationResult } = require('express-validator');
 const router = express.Router();
 
-// @route   GET api/users
-// @desc    Test route
+// @route   POST api/users
+// @desc    Register user
 // @access  Public
-router.get('/', (req, res) => res.send('User Route'));
+router.post('/', [
+  check('name', 'Name is Required').not().isEmpty(),
+  check('email', 'Please include a valid email').isEmail(),
+  check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6 })
+], 
+(req, res) => {
+  const errors = validationResult(req);
+  if(!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  // See if user exists
+
+  // Get users gravatar
+
+  // Encrypt password with bcrypt
+
+  // Return jsonwebtoken
+  
+  console.log(req.body);
+  res.send('User Route');
+});
 
 module.exports = router;
--- mern/server.js
+++ mern/server.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const { check, validationResult } = require('express-validator');
 
 const app = express();
 
 connectDB();
+
+// Init Middleware
+app.use(express.json({ extended: false }));
 
 app.get('/', (req, res) => res.send('API Running'));
 
