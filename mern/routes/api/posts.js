bc6d575a5fbc6d8f04e646fd8d7e0cb4935219e0
Chad Palmer <chad.n.palmer@gmail.com>
2021-09-20 16:43:00 -0500

    users can now like and unlike posts

--- mern/routes/api/posts.js
+++ mern/routes/api/posts.js
@@ -131,4 +131,33 @@ router.put('/like/:id', auth, async (req, res) => {
   }
 });
 
+
+// @route   PUT api/posts/unlike/:id
+// @desc    Unlike a post
+// @access  Private
+router.put('/unlike/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    // Check if post has already been liked
+    if (post.likes.filter(like => like.user.toString() === req.user.id).length === 0) {
+      return res.status(400).json({ msg: 'Post has not been liked' });
+    }
+
+    // Get remove index
+    const removeIndex = post.likes.map(like => like.user.toString()).indexOf(req.user.id);
+    
+    // Remove the like at specified index
+    post.likes.splice(removeIndex, 1);
+
+    // Save/update post in database and return all post likes
+    await post.save();
+    res.json(post.likes);
+
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('Server Error');
+  }
+});
+
 module.exports = router;
