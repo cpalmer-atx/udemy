a8d2de60b510c90d057c4996485a38e9b44d3377
Chad Palmer <chad.n.palmer@gmail.com>
2021-09-20 08:40:33 -0500

    completed profile route/controller functionality

--- mern/routes/api/profile.js
+++ mern/routes/api/profile.js
@@ -306,8 +306,20 @@ router.delete('/education/:edu_id', auth, async (req, res) => {
 router.get('/github/:username', (req, res) => {
   try {
     const options = {
-      
+      uri: `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc&client_id=${config.get('githubClientId')}&client_secret=${config.get('githubSecret')}`,
+      method: 'GET',
+      headers: { 'user-agent': 'node.js' }
     }
+
+    request(options, (error, response, body) => {
+      if (error) console.error(error);
+
+      if(response.statusCode !== 200) {
+        return res.status(404).json({ msg: 'No Github profile found' });
+      }
+      res.json(JSON.parse(body));
+    });
+
   } catch (error) {
     console.error(error.message);
     res.status(500).send('Server Error');
