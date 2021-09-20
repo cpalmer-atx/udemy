47fb436b5552d8d522febc4c012919a91450b98a
Chad Palmer <chad.n.palmer@gmail.com>
2021-09-20 17:07:31 -0500

    users can now delete their comments on posts

--- mern/routes/api/posts.js
+++ mern/routes/api/posts.js
@@ -196,4 +196,42 @@ router.post('/comment/:id', [auth, [
   }
 );
 
+
+// @route   DELETE api/posts/comment/:id/:comment_id
+// @desc    Delete comment
+// @access  Private
+router.delete('/comment/:id/:comment_id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    // Pull comment from post
+    const comment = post.comments.find(comment => comment.id === req.params.comment_id);
+
+    // Verify comment exists
+    if (!comment) {
+      return res.status(404).json({ msg: 'Comment does not exist' });
+    }
+
+    // Check user
+    if (comment.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: 'User not authorized' });
+    }
+
+    // Get remove index
+    const removeIndex = post.comments.map(comment => comment.user.toString()).indexOf(req.user.id);
+    
+    // Remove the like at specified index
+    post.comments.splice(removeIndex, 1);
+
+    // Save/update post in database and return all post likes
+    await post.save();
+    res.json(post.comments);
+
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+
 module.exports = router;
