084725114dde2bf0182f439977c68301690da773
Chad Palmer <chad.n.palmer@gmail.com>
2021-09-21 18:39:20 -0500

    example call to backend with axios commented out, preparing to set up redux

--- mern/client/src/components/auth/Register.js
+++ mern/client/src/components/auth/Register.js
@@ -1,4 +1,5 @@
 import { Fragment, useState } from "react";
+// import axios from 'axios';
 
 const Register = () => {
   const [ formData, setFormData ] = useState({
@@ -11,12 +12,32 @@ const Register = () => {
 
   const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value });
 
-  const onSubmit = e => {
+  const onSubmit = async e => {
     e.preventDefault();
     if (password !== password2) { 
       console.log('Passwords do not match')
     } else {
-      console.log(formData);
+      console.log('SUCCESS');
+      // const newUser = {
+      //   name,
+      //   email,
+      //   password
+      // }
+
+      // try {
+      //   const config = {
+      //     headers: {
+      //       'Content-Type': 'application/json'
+      //     }
+      //   }
+
+      //   const body = JSON.stringify(newUser);
+      //   const res = await axios.post('/api/users', body, config);
+      //   console.log(res.data);
+
+      // } catch (error) {
+      //   console.error(error.response.data);
+      // }
     }
   }
 
